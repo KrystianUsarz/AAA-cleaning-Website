df7cde78d67124d92e3efdcfd12879faf6102874
Krystian Usarz <Krystian.Usarz@sage.com>
2021-12-03 15:52:00 +0100

    start coding

---
+++ view/view-header-nav.php
@@ -0,0 +1,12 @@
+<div>
+    <h2>AAA-Cleaning</h2>
+</div>
+
+<div>
+    <ul>
+        <li>Homepage</li>
+        <li>Angebote</li>
+        <li>jetzt Buchen</li>
+        <li>Adminbereich</li>
+    </ul>
+</div>
---
+++ view/view-homepage.php
@@ -0,0 +1,19 @@
+<?php include './model/model-homepage.php'?>
+
+<!DOCTYPE html>
+<html>
+
+    <head>
+        <meta charset='utf-8'>
+        <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+        <title>Krystians Blog</title>
+        <meta name='viewport' content='width=device-width, initial-scale=1'>
+        <link rel="stylesheet" href="./css/style-homepage.css">
+        <link rel="stylesheet" href="./css/style-header-nav.css">
+    </head>
+
+    <body>
+        <?php include 'view-header-nav.php'?>
+
+    </body>
+</html>
