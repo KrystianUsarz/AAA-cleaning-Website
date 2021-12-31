091483379d2a1b7f04178b072eebb2a4bd3b8dda
Krystian Usarz <Krystian.Usarz@sage.com>
2021-12-31 18:36:58 +0100

    Ausloggen von adminbereich fertig

---
+++ health/view/view-admin-homepage.php
@@ -0,0 +1,14 @@
+<?php 
+    include './html-header.php';
+    include 'view-admin-nav.php';
+?>
+
+
+    <body>
+        <div>
+            <p>Zum ausloggen drücken sie bitte den Knopf Ausloggen</p>
+            <a href="index.php?page=home">Ausloggen</a>
+        </div>
+        
+    </body>
+</html>
---
+++ health/view/view-admin-nav.php
@@ -0,0 +1,35 @@
+<!-- header -->
+<header>
+        <!-- header inner -->
+        <div class="header">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xl-3 col-lg-3 col-md-3 col-sm-3 col logo_section">
+                        <div class="full">
+                            <div class="center-desk">
+                                <div class="logo"> <a href="index.html"><img src="images/firmalogo.jpg" alt="#"></a> </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-9 col-lg-9 col-md-9 col-sm-9">
+                        <div class="menu-area">
+                            <div class="limit-box">          
+                                <nav class="main-menu">
+                                    
+                                    <ul class="menu-area-main">
+                                        <li class="active"> <a href="">Ausloggen</a> </li>
+                                        <li> <a href="">Homepage verwalten</a> </li>
+                                        <li> <a href="">Angebote verwalten</a> </li>
+                                        <li> <a href="">Kalender</a> </li>
+                                        <li> <a href="">Kunden</a> </li>
+                                    </ul>
+                                </nav>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- end header inner -->
+</header>
+<!-- end header -->
